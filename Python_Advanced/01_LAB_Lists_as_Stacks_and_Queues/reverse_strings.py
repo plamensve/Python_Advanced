39e0dde312cbc0aea1521a2d965615ea2a5dd793
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-01-09 19:02:05 +0200

    Python Advanced - 09.01.2024

--- Python_Advanced/01_LAB_Lists_as_Stacks_and_Queues/reverse_strings.py
+++ Python_Advanced/01_LAB_Lists_as_Stacks_and_Queues/reverse_strings.py
@@ -1,8 +1,15 @@
-string_line = input()
-new_string = ''
-for index in range(len(string_line), -1, -1):
-    if index == 0:
-        break
-    new_string += string_line[index - 1]
-print(new_string)
-# testTestTestTestTestTestTestTestТestTestTestTestTestTestTestТестТестТестTestТестТестТестTestTestTestТестTeest
+# string_line = input()
+# new_string = ''
+# for index in range(len(string_line), -1, -1):
+#     if index == 0:
+#         break
+#     new_string += string_line[index - 1]
+# print(new_string)
+
+string_line = list(input())
+my_stack = []
+
+while string_line:
+    my_stack.append(string_line.pop())
+
+print("".join(my_stack))
